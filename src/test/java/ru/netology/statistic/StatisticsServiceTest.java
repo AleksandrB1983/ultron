d493b5597a0a468d68504a918d7b3e2829e30bc4
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2023-10-10 11:42:51 +0700

    sixth commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.statistic;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
